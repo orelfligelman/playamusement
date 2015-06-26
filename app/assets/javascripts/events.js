12fe7ea6a7d77667360532b9b3a8895c97d9673f
Orel Fligelman <orelmfligelman@gmail.com>
2015-06-26 13:27:19 -0400

    added some javascript code

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,3 +1,13 @@
+
+$(document).ready(function() {
+    $('#calendar').fullCalendar({
+        googleCalendarApiKey: '',
+        events: {
+            googleCalendarId: '754qcnsp17sgll46iduflhbem0@group.calendar.google.com'
+        }
+    });
+});
+
 $(function() {
     $( "#datepicker" ).datepicker();
  });
